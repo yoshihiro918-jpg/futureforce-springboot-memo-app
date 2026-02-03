c1837d2f6e9a5346dda64f148ff759b54e3782ea
yoshihiro <yoshihiro@localhost>
2026-02-03 23:11:56 +0900

    メモに優先順位機能追加

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
@@ -35,21 +34,6 @@ public class MemoController {
         List<Memo> memos = memoRepository.findAllByOrderByPriorityAscIdDesc();
         model.addAttribute("memos", memos);
         return "memo-list";
-    }
-    
-    @GetMapping("/search")
-    public String search(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
-        List<Memo> memos;
-
-        if (keyword != null && !keyword.isEmpty()) {
-            memos = memoRepository.findByTitleContainingOrderByPriorityAscIdDesc(keyword);
-        } else {
-            return "redirect:/memo"; 
-        }
-
-        model.addAttribute("memos", memos);
-        model.addAttribute("keyword", keyword);
-        return "memo-list"; 
     }
 
     @GetMapping("/new")
